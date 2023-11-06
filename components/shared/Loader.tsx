6f14f3e2eb3ce043e1bb1b143f4b5f81c00d0eb5
codetective <iaminiodu@gmail.com>
2023-11-06 21:27:25 +0100

    fix import

--- components/shared/Loader.tsx
+++ components/shared/Loader.tsx
@@ -1,16 +1,15 @@
-import Center from '@/components/Center';
 import React from 'react';
 
 function Loader() {
   return (
-    <Center className='h-[100vh] w-full'>
+    <div className='h-[100vh] w-full flex items-center justify-center'>
       <div className='lds-ellipsis'>
         <div></div>
         <div></div>
         <div></div>
         <div></div>
       </div>
-    </Center>
+    </div>
   );
 }
 
