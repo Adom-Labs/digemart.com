01bb063f5126baa0ef6f7020bc4ceee8e5540c18
codetective <iaminiodu@gmail.com>
2023-12-17 18:19:10 +0100

    feat: modified nav

--- components/main/navigation/TopNav.tsx
+++ components/main/navigation/TopNav.tsx
@@ -7,7 +7,11 @@ import WrapContent from '@/components/shared/WrapContent';
 import AppLogo from '@/components/shared/AppLogo';
 import ThemeToggle from '@/components/shared/theme/ThemeToggle';
 import { useTheme } from '@/providers/ThemeProvider';
-import { OpenAgentModal } from '../homepage/CallToAction';
+// import { OpenAgentModal } from '../homepage/CallToAction';
+import { FaPlug } from 'react-icons/fa';
+import { FaShoppingBag } from 'react-icons/fa';
+import { FaRegQuestionCircle } from 'react-icons/fa';
+import { RiMenu3Line } from 'react-icons/ri';
 
 function TopNav() {
   const { theme } = useTheme();
@@ -16,11 +20,31 @@ function TopNav() {
       <WrapContent>
         <div className='flex items-center justify-between  py-3 shadow-sm '>
           <AppLogo logoType={theme === 'night' ? 'n' : 'd'} />
-          <div className='flex items-center gap-5 '>
-            <div className='hidden md:block'>
+          <div className='flex items-center gap-3 md:gap-5 '>
+            <Link
+              title='find people to buy things from'
+              href='/plugs'
+              className={
+                linkStyle + 'hidden text-lg sm:flex items-center gap-1'
+              }
+            >
+              Find Your Plug
+              <FaPlug />
+            </Link>
+            <Link
+              title='find people to buy things from'
+              href='#!'
+              className={
+                linkStyle + 'hidden text-lg md:flex items-center gap-1'
+              }
+            >
+              Find Stores
+              <FaShoppingBag />
+            </Link>
+            {/* <div className='hidden md:block'>
               <NavLink title='Start Selling' path='#sell' />
-            </div>
-            <div onClick={OpenAgentModal} className='hidden md:block'>
+            </div> */}
+            {/* <div onClick={OpenAgentModal} className='hidden md:block'>
               <a
                 title='Become an Agent'
                 href='#!'
@@ -28,9 +52,10 @@ function TopNav() {
               >
                 Become an Agent
               </a>
-            </div>
-            <div className='hidden md:block'>
+            </div> */}
+            <div className='hidden md:flex items-center gap-1'>
               <NavLink title='FAQ' path='#FAQ' />
+              <FaRegQuestionCircle />
             </div>
 
             <div title='profile'>
@@ -38,8 +63,50 @@ function TopNav() {
                 <BiUserCircle className='text-4xl' />
               </Link>
             </div>
-            <div title='profile'>
+            <div title='toggle theme'>
               <ThemeToggle />
+            </div>
+            <div title='toggle menu'>
+              <div>
+                <details className='dropdown dropdown-end'>
+                  <summary className='m-1 btn btn-xs bg-transparent hover:bg-transparent outline-none border-0'>
+                    <RiMenu3Line className='text-3xl hover:text-purple-500 cursor-pointer ' />
+                  </summary>
+                  <ul className='p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52'>
+                    <li>
+                      <Link
+                        title='find people to buy things from'
+                        href='/plugs'
+                        className={
+                          linkStyle + ' text-lg flex items-center gap-1'
+                        }
+                      >
+                        Find Your Plug
+                        <FaPlug />
+                      </Link>
+                    </li>
+
+                    <li>
+                      <Link
+                        title='find people to buy things from'
+                        href='#!'
+                        className={
+                          linkStyle + ' text-lg flex items-center gap-1'
+                        }
+                      >
+                        Find Stores
+                        <FaShoppingBag />
+                      </Link>
+                    </li>
+                    <li>
+                      <div className='flex items-center gap-1'>
+                        <NavLink title='FAQ' path='#FAQ' />
+                        <FaRegQuestionCircle />
+                      </div>
+                    </li>
+                  </ul>
+                </details>
+              </div>
             </div>
           </div>
         </div>
---
+++ pages/plugs/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Plugs() {
+  return <div>Plugs</div>;
+}
+
+export default Plugs;
